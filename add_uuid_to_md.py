14a4ece5c9fea13fc709ed85a8ff524cdfb72b73
Marceau-h <git@marceau-h.fr>
2024-09-12 18:37:10 +0200

    action

--- add_uuid_to_md.py
+++ add_uuid_to_md.py
@@ -1,7 +1,7 @@
 from os import popen
 from pathlib import Path
 
-command = r"""uuid=$(uuidgen); sed -i '0,/---/{s/---/---\nuuid: '"$uuid"'\n/}' """
+command = r"""uuid=$(uuidgen); sed -i '0,/---\n/{s/---\n/---\nuuid: '"$uuid"'\n/}' """
 
 def recur_find_md_files(path):
     for p in path.iterdir():
